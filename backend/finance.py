21b61a1fa23ae8550a6a82e5012f4306406218e0
Nico Plattner <nicoplattner95@gmail.com>
2024-10-25 20:20:25 +0200

    financial calculations

--- backend/finance.py
+++ backend/finance.py
@@ -1,7 +1,17 @@
-def finance():
+def finance(roof_area, sunlight_hours):
     kpi = dict()
 
-    kpi['roi_in_days'] = 600
-    kpi['costs_of_investment'] = 6000_00
-    
+    system_efficiency = 0.15
+    electricity_price = 0.30
+    average_installation_cost = 10000
+    lifetime_panel = 23
+
+    annual_savings = roof_area * system_efficiency * sunlight_hours * electricity_price
+    break_even = average_installation_cost / annual_savings
+    roi = (((annual_savings * lifetime_panel) - average_installation_cost) / average_installation_cost ) * 100
+
+    kpi['annual_savings'] = annual_savings
+    kpi['break_even_after'] = break_even
+    kpi['roi'] = roi
+
     return kpi
