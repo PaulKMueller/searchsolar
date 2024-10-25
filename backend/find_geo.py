4bb457a5025748832cd614f2663631a79220d72e
PaulKMueller <paul.mueller@quantco.com>
2024-10-25 19:52:27 +0200

    Design

--- backend/find_geo.py
+++ backend/find_geo.py
@@ -2,6 +2,8 @@ import requests
 from area import area
 
 def find_geo(plz, city, street, house_number) -> dict:
+
+
     url = "https://overpass-api.de/api/interpreter"
     query = f"""
         [out:json];
@@ -14,10 +16,12 @@ def find_geo(plz, city, street, house_number) -> dict:
         >>;
         out skel;
     """
-    response = requests.get(url, params={'data': query})
-    data = response.json()
 
-    print(data)
+    if plz == "76137" and city == "Karlsruhe" and street == "Morgenstraße" and house_number == "5":
+        data = {'version': 0.6, 'generator': 'Overpass API 0.7.62.1 084b4234', 'osm3s': {'timestamp_osm_base': '2024-10-25T17:43:41Z', 'copyright': 'The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.'}, 'elements': [{'type': 'way', 'id': 97892155, 'nodes': [1133026383, 1133026015, 1133026344, 4301480944, 4301480945, 4301480939, 4301480938, 1133026215, 1133025932, 1133026383], 'tags': {'addr:city': 'Karlsruhe', 'addr:country': 'DE', 'addr:housenumber': '5', 'addr:postcode': '76137', 'addr:street': 'Morgenstraße', 'building': 'apartments', 'building:levels': '4', 'roof:levels': '2', 'roof:shape': 'gabled', 'source': 'LA-KA'}}, {'type': 'node', 'id': 1133025932, 'lat': 49.0024739, 'lon': 8.4133456}, {'type': 'node', 'id': 1133026015, 'lat': 49.0026068, 'lon': 8.4132066}, {'type': 'node', 'id': 1133026215, 'lat': 49.0024745, 'lon': 8.4133731}, {'type': 'node', 'id': 1133026344, 'lat': 49.0026102, 'lon': 8.4133644}, {'type': 'node', 'id': 1133026383, 'lat': 49.0024711, 'lon': 8.4132132}, {'type': 'node', 'id': 4301480938, 'lat': 49.0025218, 'lon': 8.4133708}, {'type': 'node', 'id': 4301480939, 'lat': 49.0025222, 'lon': 8.4133907}, {'type': 'node', 'id': 4301480944, 'lat': 49.0025592, 'lon': 8.4133669}, {'type': 'node', 'id': 4301480945, 'lat': 49.0025597, 'lon': 8.4133889}]}
+    else:
+        response = requests.get(url, params={'data': query})
+        data = response.json()
 
     # Initialize the geo dictionary with default values
     geo = {
@@ -33,5 +37,13 @@ def find_geo(plz, city, street, house_number) -> dict:
 
     geo['squaremeters'] = roofarea
     geo['outline'] = lat_lon_list
+    print(f"Lat lon list: {lat_lon_list}")
+
+    geo['longitude'] = lat_lon_list[0][1]
+
+    print(f"Longitude: {geo['longitude']}")
+
+    geo['latitude'] = lat_lon_list[0][0]
+    print(f"Latitude: {geo['latitude']}")
 
     return geo
--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from backend.find_geo import find_geo
 from backend.sunshine import get_annual_sunshine_hours
 import streamlit as st
 from backend.orchestrator import get_potential_profits
+import pandas as pd
 
-st.title("Address Form")
-st.write("We can tell you how profitable solar panels are with your roof!")
 
-postal_code = st.text_input("Postal Code")
-city = st.text_input("City")
-street = st.text_input("Street")
-house_number = st.text_input("House Number")
+st.sidebar.title("Address Input")
+
+postal_code = st.sidebar.text_input("Postal Code", value="76137")
+city = st.sidebar.text_input("City", value="Karlsruhe")
+street = st.sidebar.text_input("Street", value="Morgenstraße")
+house_number = st.sidebar.text_input("House Number", value="5")
 
 # Submit button
-if st.button("Submit"):
+if st.sidebar.button("Submit"):
     if postal_code and city and street and house_number:
         geo = find_geo(postal_code, city, street, house_number)
         potential_profits = get_potential_profits(postal_code, city, street, house_number)
         sun_hours = get_annual_sunshine_hours(f"{street} {house_number} {postal_code} {city}") 
 
-        st.title("Here is your result")
-        st.write("Enter your address, and we’ll tell you how much you could earn with solar panels!")
-
+        st.title("Here is your result:")
         st.write(f"Roof Outline: {geo['outline']}")
         st.write(f"Square Meters: {geo['squaremeters']}")
         st.write(f"Estimated Sun Hours: {sun_hours} hours")
         st.write(f"Potential Profit (KPI): {potential_profits} €")
+        latitude = geo['outline'][0][0]
+        longitude = geo['outline'][0][1]
+
+        df = pd.DataFrame({'latitude': [latitude], 'longitude': [longitude]})
+        st.map(df)
     else:
         st.warning("Please enter a complete address before submitting.")
+else:
+    st.map()
